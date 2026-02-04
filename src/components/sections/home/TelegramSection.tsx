cea8b459b918c86e9316c6827e865c661eac2e11
Nikolay <nikolayzhidkov@icloud.com>
2026-02-04 21:47:15 +0700

    100

--- src/components/sections/home/TelegramSection.tsx
+++ src/components/sections/home/TelegramSection.tsx
@@ -4,7 +4,7 @@ import s from "@/app/(home)/Home.module.scss";
 
 export const TelegramSection = () => {
   return (
-    <section className={clsx(s.tg_home, "sectiob-padding")}>
+    <section className={clsx(s.tg_home, "section-padding")}>
       <div className="wrapper">
         <div>
           <h2>Канал «ГРИБАНОВ»</h2>
